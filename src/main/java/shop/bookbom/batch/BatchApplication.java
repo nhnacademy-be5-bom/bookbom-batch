435783053b5d14fa0f5f6d7a25a962f13abc2d54
Yejin Song <songyejin2000@gmail.com>
2024-04-22 16:53:26 +0900

    [feat] 생일 쿠폰 batch 구현

--- src/main/java/shop/bookbom/batch/BatchApplication.java
+++ src/main/java/shop/bookbom/batch/BatchApplication.java
@@ -1,10 +1,14 @@
 package shop.bookbom.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableBatchProcessing
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
